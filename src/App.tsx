acccdf82549a3b4fd59906c7633e7014a253ed67
Lucca Gabriel <luccavitalc@gmail.com>
2023-01-31 17:08:27 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
+import React, { useState } from 'react';
+
 const App = () => {
+  let [name, setName] = useState('');
+  let [sobreName, setSobreName] = useState('');
+  let [idade, setIdade] = useState('');
+
+  const handleName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  }
+
+  const handleSobreName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSobreName(event.target.value);
+  }
+
+  const handleIdade = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIdade(event.target.value);
+  }
+
   return (
     <div>
       <div>
         Nome:
-        <input type="text" />
+        <input type="text" value={name} onChange={handleName}/>
       </div>
 
       <div>
-        Sobrenome:
-        <input type="text" />
+        Sobrenome: {sobreName}
+        <input type="text" value={sobreName} onChange={handleSobreName}/>
       </div>
 
       <div>
-        Idade:
-        <input type="text" />
+        Idade: {idade}
+        <input type="text" value={idade} onChange={handleIdade}/>
       </div>
 
       <hr/>
 
-      Olá ---, tudo bem?<br/>
-      Você tem -- anos.
+      Olá {name} {sobreName}, tudo bem?<br/>
+      Você tem {idade} anos.
     </div>
   );
 }
